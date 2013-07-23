bf041d2aa4ea50093dc39e51abe99a695a00f037
philipz <philipzheng@gmail.com>
2013-07-23 10:14:13 +0800

    Add Lib

--- src/ExampleFeatureSurf.java
+++ src/ExampleFeatureSurf.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2011-2012, Peter Abeles. All Rights Reserved.
+ * Copyright (c) 2011-2013, Peter Abeles. All Rights Reserved.
  *
  * This file is part of BoofCV (http://boofcv.org).
  *
@@ -19,7 +19,9 @@
 
 
 import boofcv.abst.feature.detdesc.DetectDescribePoint;
-import boofcv.abst.feature.detect.extract.FeatureExtractor;
+import boofcv.abst.feature.detect.extract.ConfigExtract;
+import boofcv.abst.feature.detect.extract.NonMaxSuppression;
+import boofcv.abst.feature.detect.interest.ConfigFastHessian;
 import boofcv.abst.feature.orientation.OrientationIntegral;
 import boofcv.alg.feature.describe.DescribePointSurf;
 import boofcv.alg.feature.detect.interest.FastHessianFeatureDetector;
@@ -32,6 +34,7 @@ import boofcv.factory.feature.orientation.FactoryOrientationAlgs;
 import boofcv.io.image.UtilImageIO;
 import boofcv.struct.feature.ScalePoint;
 import boofcv.struct.feature.SurfFeature;
+import boofcv.struct.image.ImageDataType;
 import boofcv.struct.image.ImageFloat32;
 import boofcv.struct.image.ImageSingleBand;
 
@@ -47,45 +50,46 @@ public class ExampleFeatureSurf {
 
 	/**
 	 * Use generalized interfaces for working with SURF.  This removes much of the drudgery, but also reduces flexibility
-	 * and slightly increases memory and computational requirements.  For example, the integral image is computed twice.
+	 * and slightly increases memory and computational requirements.
 	 * 
 	 *  @param image Input image type. DOES NOT NEED TO BE ImageFloat32, ImageUInt8 works too
 	 */
 	public static void easy( ImageFloat32 image ) {
 		// create the detector and descriptors
-		DetectDescribePoint<ImageFloat32,SurfFeature>
-				surf = FactoryDetectDescribe.surf(0, 2, 200, 2, 9, 4, 4, true, ImageFloat32.class);
+		DetectDescribePoint<ImageFloat32,SurfFeature> surf = FactoryDetectDescribe.
+			surfStable(new ConfigFastHessian(0, 2, 200, 2, 9, 4, 4), null, null, ImageFloat32.class);
 
 		 // specify the image to process
 		surf.detect(image);
 
 		System.out.println("Found Features: "+surf.getNumberOfFeatures());
-		System.out.println("First descriptor's first value: "+surf.getDescriptor(0).value[0]);
+		System.out.println("First descriptor's first value: "+surf.getDescription(0).value[0]);
 	}
 
 	/**
-	 * Configured exactly the same as the easy example above, but require a lot more code and a more in depth understanding
-	 * of how SURF works and is configured.  Instead of TupleDesc_F64, SurfFeature are computed in this case.  They are
-	 * almost the same as TupleDesc_F64, but contain the Laplacian's sign which can be used to speed up association.
-	 * That is an example of how using less generalized interfaces can improve performance.
+	 * Configured exactly the same as the easy example above, but require a lot more code and a more in depth
+	 * understanding of how SURF works and is configured.  Instead of TupleDesc_F64, SurfFeature are computed in
+	 * this case.  They are almost the same as TupleDesc_F64, but contain the Laplacian's sign which can be used
+	 * to speed up association. That is an example of how using less generalized interfaces can improve performance.
 	 * 
 	 * @param image Input image type. DOES NOT NEED TO BE ImageFloat32, ImageUInt8 works too
 	 */
 	public static <II extends ImageSingleBand> void harder( ImageFloat32 image ) {
 		// SURF works off of integral images
 		Class<II> integralType = GIntegralImageOps.getIntegralType(ImageFloat32.class);
-		
+
 		// define the feature detection algorithm
-		FeatureExtractor extractor = FactoryFeatureExtractor.nonmax(2, 0, 5, true);
+		NonMaxSuppression extractor =
+				FactoryFeatureExtractor.nonmax(new ConfigExtract(2, 0, 5, true));
 		FastHessianFeatureDetector<II> detector = 
 				new FastHessianFeatureDetector<II>(extractor,200,2, 9,4,4);
 
 		// estimate orientation
 		OrientationIntegral<II> orientation = 
-				FactoryOrientationAlgs.sliding_ii(0.65, Math.PI / 3.0, 8, -1, 6, integralType);
+				FactoryOrientationAlgs.sliding_ii(null, integralType);
 
-		DescribePointSurf<II> descriptor = FactoryDescribePointAlgs.<II>msurf(integralType);
-		
+		DescribePointSurf<II> descriptor = FactoryDescribePointAlgs.<II>surfStability(null,integralType);
+
 		// compute the integral image of 'image'
 		II integral = GeneralizedImageOps.createSingleBand(integralType,image.width,image.height);
 		GIntegralImageOps.transform(image, integral);
@@ -104,7 +108,7 @@ public class ExampleFeatureSurf {
 			// estimate orientation
 			orientation.setScale(p.scale);
 			double angle = orientation.compute(p.x,p.y);
-			
+
 			// extract the SURF description for this region
 			SurfFeature desc = descriptor.createDescription();
 			descriptor.describe(p.x,p.y,angle,p.scale,desc);
@@ -112,20 +116,20 @@ public class ExampleFeatureSurf {
 			// save everything for processing later on
 			descriptions.add(desc);
 		}
-		
+
 		System.out.println("Found Features: "+points.size());
 		System.out.println("First descriptor's first value: "+descriptions.get(0).value[0]);
 	}
 
 	public static void main( String args[] ) {
-		
-		ImageFloat32 image = UtilImageIO.loadImage("particles01.jpg",ImageFloat32.class);
-		
+
+		ImageFloat32 image = UtilImageIO.loadImage("../data/evaluation/particles01.jpg",ImageFloat32.class);
+
 		// run each example
 		ExampleFeatureSurf.easy(image);
 		ExampleFeatureSurf.harder(image);
-		
+
 		System.out.println("Done!");
-		
+
 	}
 }
